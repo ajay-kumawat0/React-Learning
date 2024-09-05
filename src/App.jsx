8b86c93c6f6f0359bbce644eeacf596f04621d1c
Ajay Kumawat <ajay@elemensissoftech.com>
2024-09-05 15:36:50 +0530

    learn hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-function App() {
+import { useState } from "react";
 
+function App() {
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    if(counter === 20) {
+      return alert('Counter is already at max value');
+    } else {
+      setCounter(counter+1);
+    }
+  }
+
+  const removeValue = () => {
+    if(counter === 0) {
+      return alert('Counter is already at min value'); 
+    } else {
+      setCounter(counter-1);
+    }
+  }
 
   return (
-    <h1>React</h1>
+    <>
+      <h1>React App: {counter}</h1>
+      <h3>Counter: {counter}</h3>
+      <button onClick={addValue}>Increase</button>
+      <button onClick={removeValue}>Decrease</button>
+    </>
   )
 }
 
-export default App
+export default App;
